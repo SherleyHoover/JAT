05afa5c9d7ba1ac13d9064951d86a01b811efb99
SherleyHoover <3030756558@qq.com>
2026-08-19 15:46:44 +0800

    allow frontend CORS origin

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,8 @@ MIDDLEWARE = [
 ]
 CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://jat-urt.onrender.com",
 ).split(",")
 ROOT_URLCONF = 'config.urls'
 
